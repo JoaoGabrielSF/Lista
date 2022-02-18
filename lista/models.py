5d338b37e7cdc450b4e93f30e42731d0f44bc2eb
João Gabriel <joaogabrielsantosfernandes@gmail.com>
2022-02-18 08:28:47 -0300

    ...

--- lista/models.py
+++ lista/models.py
@@ -1,15 +1,12 @@
 from datetime import datetime
-from enum import unique
-import random
-from time import timezone
+import uuid
 from django.db import models
-from django.forms import CharField
 from django.test import client
 
 
 class tabela_cliente(models.Model):
     
-    id_cliente = random.sample(range(0,100000), 1) 
+    id_cliente = uuid.uuid4()
     nome = models.CharField(max_length=50) 
     cpf = models.CharField(max_length=11)
     telefone = models.CharField(max_length=14)
@@ -18,13 +15,13 @@ class tabela_cliente(models.Model):
     cidade = models.CharField(max_length=50)
     estado = models.CharField(max_length=50)
 
-def str(self):
-        return self.nome, self.id_cliente
+    def str(self):
+        return self.nome
 
 
 
 class tabela_produto (models.Model):
-    id_produto = random.sample(range(0,100000),1)
+    id_produto = uuid.uuid4()
     nome_produto = models.CharField( max_length=50)
     quantidade = models.CharField(max_length=50) 
     preco_unidade = models.CharField(("Preço_unidade $"),max_length=50)
@@ -38,14 +35,12 @@ def str(self):
 
 class tabela_venda(models.Model):
         
-        tabela_id = models.ForeignKey(tabela_cliente,verbose_name=("id_cliente"), on_delete=models.CASCADE)
-        id_venda = random.sample(range(0,100000),1)
-        dia_venda = datetime.today()
-        
+    tabela_id = models.ForeignKey(tabela_cliente, on_delete=models.CASCADE,blank=False)
+    id_venda = uuid.uuid4()
+    dia_venda = datetime.today()
+
 
 def str(self):
     return self.id_venda , self.dia_venda
 
 
-#class venda_possui_produto(models.Model):
-#   pass
